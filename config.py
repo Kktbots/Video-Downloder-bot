1d131c493cc80441eff06a911994b87977e6b185
Kktbots <143538428+Kktbots@users.noreply.github.com>
2023-09-07 21:48:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS = """"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6641479588:AAGvEBQvRg-Fv1dT6dWBE-S6F5zqeGd9tpU")
+    API_ID = int(os.environ["API_ID", 29540156]
+    API_HASH = os.environ["API_HASH", "775d47859519f34f01549344a392280b"]
+    AUTH_USERS = ""5943972790""
 
